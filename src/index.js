8b308e65ff32e5e39b764a69c339b20beabec7cd
nadezhdaGol <78170166+nadezhdaGol@users.noreply.github.com>
2021-02-20 14:50:16 +0300

    feat: add solution of the task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (array == false || array == null) {
+        return 0;
+    } else {
+        array.sort((a, b) => {
+            return a - b;
+        });
+        return array[0];
+    }
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (array == false || array == null) {
+        return 0;
+    } else {
+        array.sort((a, b) => {
+            return b - a;
+        });
+        return array[0];
+    }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (array == false || array == null) {
+        return 0;
+    } else {
+        let averageNum = array.reduce((total, amount, index, arr) => {
+            total += amount;
+            if (index === arr.length - 1) {
+                return total / arr.length;
+            } else {
+                return total;
+            }
+
+        });
+        return averageNum;
+    }
 }
